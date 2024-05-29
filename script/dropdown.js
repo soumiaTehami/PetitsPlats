e762f43e12f0012b0546c5f4c4ab66f868c88aa0
tehami soumia <sou_mia_46@hotmail.fr>
2024-05-29 05:54:51 +0200

    msg erreur

--- script/dropdown.js
+++ script/dropdown.js
@@ -203,14 +203,19 @@ document.addEventListener("DOMContentLoaded", function() {
  export function updateRecipeCount() {
     // Select the element that will display the number of recipes
     const recipesNumberElement = document.querySelector('.recipesNumber');
-  
+    
     // Select all .recipe-card elements
     const allRecipes = document.querySelectorAll('.recipe-card');
+    const recipeContainer = document.getElementById('recipeList');
+    // Get the search input value
+  const searchInput = document.querySelector('#searchInput'); // Assuming there's an input with this ID
+  const searchTerm = searchInput ? searchInput.value : 'XXX';
   
     // Update the .recipesNumber element with the number of recipes
     if (allRecipes.length === 0) {
-      recipesNumberElement.textContent = '0 recette';
-    } else if (allRecipes.length === 1) {
+        recipesNumberElement.textContent = ''; // Clear the previous count message if no recipes are found
+        recipeContainer.innerHTML = `<p>Aucune recette ne contient ‘${searchTerm}’, vous pouvez chercher « tarte aux pommes », « poisson », etc.</p>`;
+      } else if (allRecipes.length === 1) {
       recipesNumberElement.textContent = '1 recette';
     } else {
       recipesNumberElement.textContent = allRecipes.length + ' recettes';
